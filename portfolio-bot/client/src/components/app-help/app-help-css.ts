44e31232860bcde7ba8b41e4023fe2dd75193dcc
maelcaldas <mael@bkper.com>
2026-09-14 14:38:40 -0300

    Move Portfolio Bot to Brain repository

--- portfolio-bot/client/src/components/app-help/app-help-css.ts
+++
@@ -1,8 +0,0 @@
-import { css } from 'lit';
-
-export const appHelpCSS = css`
-    :host {
-        display: inline-flex;
-        justify-self: end;
-    }
-`;
--- portfolio-bot/client/src/index.ts
+++
@@ -1,6 +0,0 @@
-import './styles.css';
-import './web-awesome.js';
-import './components/bot-app-view.js';
-import { initBkperAPI } from './api-init.js';
-
-initBkperAPI();
